e8366c2f5df1b6fae59f5228bcc59c9f54e3b997
syahmiong <150315990+syahmiong@users.noreply.github.com>
2023-11-16 16:01:05 +0800

    Update streamlit_app.py
    Fix a typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,6 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cure.fetchone()
+my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
