691558e7b334b894f0f3da7496402818638e5af1
syahmiong <150315990+syahmiong@users.noreply.github.com>
2023-11-16 16:29:38 +0800

    Update streamlit_app.py
    Add try and catch statement to handle form changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,17 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruitvice api response
 streamlit.header('Fruitvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
 
-fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# take the json version of the response and normalize it
-fruitvice_normalized = pandas.json_normalize(fruitvice_response.json())
-# output it the screen as a table
-streamlit.dataframe(fruitvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get infromation.")
+  else:
+    fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruitvice_normalized = pandas.json_normalize(fruitvice_response.json())
+    streamlit.dataframe(fruitvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
